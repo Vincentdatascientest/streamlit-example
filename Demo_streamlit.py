8ffde7bc992899a191afe0eab3afdbfc6b5b3651
Vincentdatascientest <94071918+Vincentdatascientest@users.noreply.github.com>
2022-04-09 17:22:19 +0200

    Update Demo_streamlit.py

--- Demo_streamlit.py
+++ Demo_streamlit.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
